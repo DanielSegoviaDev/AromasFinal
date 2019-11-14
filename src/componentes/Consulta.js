fefdfb77a3fa4949f99d1ae0bfada20f4110ab41
DanielSegoviaDev <segoviadanieldev@gmail.com>
2019-11-14 16:06:31 -0300

    Esta las condiciones de el contactanos y fotos linkeadas de f y i a acomodar con css

--- src/componentes/Consulta.js
+++ src/componentes/Consulta.js
@@ -1,7 +1,45 @@
 import React from 'react';
-import Validar from '../scripts/form';
+
+
+function Validar ()
+    {
+        
+        let nombre, mail, consulta, expression, expressionA;
+        nombre = document.getElementById("nombre").value;
+        mail = document.getElementById("email").value;
+        consulta = document.getElementById("consulta").value;
+        expression = /\w+@\w+\.+[a-z]/;  
+        expressionA = /\w+@\w+\.+[a-z]\.+[a-z]/;
+
+
+        if(nombre === "" || mail === "" || consulta === ""){
+            alert("Todos los campos son obligatorios");
+            return false;
+        }
+        else if(nombre.length>30){
+            alert("El nombre es muy largo");
+            return false;
+        }
+        else if(mail.length>100){
+            alert("El EMail ingresado es muy largo");
+            return false;
+        }
+        else if(!expression.test(mail) || !expressionA.test(mail)){
+            alert("El correo no es valido");
+            return false;
+        }
+        else if(consulta.length>350){
+            alert("La consulta ingresada es muy larga, ingrese menos de 350 caracteres");
+            return false;
+        }
+
+
+            
+
+    } 
 
 function Consulta(){
+    
         
     return(
         <aside id="sidebar">
@@ -15,12 +53,13 @@ function Consulta(){
                                 <div>
                                 <label>Email</label>
                                 <input type="text" name="email" id="email" placeholder="Direccion de Email"/>
+                                <span class="error" aria-live="polite"></span>
                             </div>
                                 <div>
-                                <label>Message</label>
+                                <label>Mensaje</label>
                                 <textarea placeholder="Consulta..." name="consulta" id="consulta"></textarea>
                             </div>
-                            <button class="button_1" type="submit" id="btn" onclick={Validar}> Enviar! </button>
+                            <button class="button_1" type="submit" id="btn" onClick={Validar}> Enviar! </button>
                     </form>
                  </div>
              </aside>
--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import Facebook from '../imagenes/Facebook.png';
+import Instagram from '../imagenes/Instagram.png';
 
 function Footer(){
     return(
         <footer>
+            <a href="https://www.facebook.com/aromasdelarcoiris1/" target="_blank" rel="noopener noreferrer" id="redes-icon-f">
+            <img src={Facebook}  alt="facebook icon" />
+            </a>
             <p>Aromas del ArcoIris, Copyright &copy; 2019</p>
+            <a href="https://www.instagram.com/aromasdelarcoiris/?hl=es-la" target="_blank" rel="noopener noreferrer" id="redes-icon-i" >
+            <img src={Instagram}  alt="instagram icon"></img>
+            </a>
+
         </footer>
     )
 }
